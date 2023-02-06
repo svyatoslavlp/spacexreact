ca89bebc2d175270f743bac443b2003faff6889d
Svyatoslav <svyatoslavlp@mail.ru>
2023-02-06 13:16:57 +0500

    откатил App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import React from "react";
 import "./style.css";
 import Header from "./components/Header/Header";
 import Main from "./components/Main/Main";
@@ -6,28 +5,18 @@ import Features from "./components/Features/Features";
 import Footer from "./components/Footer/Footer";
 // import Calendar from "./components/Calendar/Calendar";
 // import Details from "./components/Details/Details";
-import FetchData from "./service/FethData";
 
-class App extends React.Component {
-  fetchData = new FetchData();
-  state = {
-    rocket: "Falcon 1",
-  };
-  componentDidMount() {
-    console.log(this.fetchData.getCompany);
-  }
-  render() {
-    return (
-      <>
-        <Header />
-        <Main rocket={this.state.rocket} />
-        <Features />
-        <Footer />
-        {/* <Calendar /> */}
-        {/* <Details /> */}
-      </>
-    );
-  }
+function App() {
+  return (
+    <>
+      <Header />
+      <Main />
+      <Features />
+      <Footer />
+      {/* <Calendar /> */}
+      {/* <Details /> */}
+    </>
+  );
 }
 
 export default App;
